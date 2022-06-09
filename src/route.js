03ea93cc88fcc7040635198e8d38f7829b86a0fb
Socootra <mriziq54@gmail.com>
2022-06-09 21:25:45 +0700

    Method GET,PUT,DELETE Added

--- src/route.js
+++ src/route.js
@@ -1,9 +1,31 @@
 /* eslint-disable linebreak-style */
-const {addNoteHandler} = require('./handler');
+const {addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler} = require('./handler');
 const routes = [{
   path: '/notes',
   method: 'POST',
   handler: addNoteHandler,
-}];
+}, {
+  path: '/notes',
+  method: 'GET',
+  handler: getAllNotesHandler,
+}, {
+  path: `/notes/{id}`,
+  method: 'GET',
+  handler: getNoteByIdHandler,
+}, {
+  path: '/notes/{id}',
+  method: 'PUT',
+  handler: editNoteByIdHandler,
+}, {
+  path: '/notes/{id}',
+  method: 'DELETE',
+  handler: deleteNoteByIdHandler,
+},
+
+];
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 /* eslint-disable linebreak-style */
-const hapi = require('@hapi/hapi');
+const Hapi = require('@hapi/hapi');
 const routes = require('./route');
 
 const init = async ()=>{
-  const server = hapi.server({
-    host: 'localhost',
+  const server = Hapi.server({
     port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
